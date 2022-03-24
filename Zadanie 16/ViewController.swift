fd7ac304a1c2486842b6a8c8bebf6c65ef20be3f
Zlobin_peter <>
2022-03-24 15:24:49 +0300

    первое изменение

---
+++ Zadanie 16/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Zadanie 16
+//
+//  Created by Петр Злобин on 23.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let a = "изменения коммит 1"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
